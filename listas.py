8a144791fc7532ebd028fecf2622e485ed8feff5
edvoidcode <edsonsilva.contato@gmail.com>
2025-04-01 22:02:38 -0300

    Adicionando conteudo de listas

---
+++ listas.py
@@ -0,0 +1,10 @@
+#listas e manipulação de dados em python
+frutas = ["maçã, melão, banana, uva, manga"]
+treino = ["peito, costa, biceps, triceps, perna"]
+semana = ["segunda, terça, quarta, quinta, sexta,"]
+dias = [1, 2, 3, 4, 5]
+
+semana.append("sabado")
+semana.append("domingo")
+for semana in semana:
+    print(semana)
